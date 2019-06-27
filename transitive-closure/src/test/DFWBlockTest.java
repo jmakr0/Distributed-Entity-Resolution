0d0c85a8ff5627c61ee72947ec420478a1318c3c
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2019-06-28 00:18:25 +0200

    third test for fw logic

--- transitive-closure/src/test/DFWBlockTest.java
+++ transitive-closure/src/test/DFWBlockTest.java
@@ -17,8 +17,8 @@ public class DFWBlockTest {
     public void before() {
         //                                  y
         //                       ------------------------------------
-        this.testMatrix[0] = new int[]{0, 1, 5, 6, 4, 8, 1, 3, 2, 4}; // |
-        this.testMatrix[1] = new int[]{9, 0, 8, 9, 4, 7, 9, 5, 3, 4}; // |
+        this.testMatrix[0] = new int[]{0, 1, 1, 3, 4, 8, 8, 2, 2, 4}; // |
+        this.testMatrix[1] = new int[]{9, 0, 2, 4, 4, 7, 9, 9, 3, 4}; // |
         this.testMatrix[2] = new int[]{7, 1, 0, 2, 1, 3, 1, 3, 4, 4}; // |  x
         this.testMatrix[3] = new int[]{6, 4, 1, 0, 5, 7, 2, 4, 4, 1}; // |
         this.testMatrix[4] = new int[]{8, 8, 6, 7, 0, 3, 9, 5, 2, 3}; // |
@@ -52,7 +52,7 @@ public class DFWBlockTest {
     }
 
     @Test
-    public void testCalculateTargetPlusTwo() {
+    public void testCalculateTargetPlusTwoInCross() {
         SubMatrix target = new SubMatrix(testMatrix, 6,6, 2);
         DFWPosition pivot = new DFWPosition(2,2);
 
@@ -70,6 +70,29 @@ public class DFWBlockTest {
         int[][] expected = new int[2][2];
         expected[0] = new int[]{0, 5};
         expected[1] = new int[]{3, 0};
+
+        Assert.assertTrue(Arrays.deepEquals(expected, targetMatrix));
+    }
+
+    @Test
+    public void testCalculateTargetPlusTwoOutOfCross() {
+        SubMatrix target = new SubMatrix(testMatrix, 0,6, 2);
+        DFWPosition pivot = new DFWPosition(2,2);
+
+        SubMatrix p1 = new SubMatrix(this.testMatrix, 0, 2, 2);
+        SubMatrix p2 = new SubMatrix(this.testMatrix, 2, 6, 2);
+
+        DFWBlock dfwBlock = new DFWBlock(target, pivot);
+        dfwBlock.addPath(p1);
+        dfwBlock.addPath(p2);
+
+        dfwBlock.calculate();
+
+        int[][] targetMatrix = dfwBlock.getTarget().getMatrix();
+
+        int[][] expected = new int[2][2];
+        expected[0] = new int[]{2, 2};
+        expected[1] = new int[]{3, 5};
 
         Assert.assertTrue(Arrays.deepEquals(expected, targetMatrix));
     }
