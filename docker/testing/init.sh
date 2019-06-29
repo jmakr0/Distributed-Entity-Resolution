7254e48518cf3d57c4ded0af884ded7ca570d635
Maximilian Kroschewski <jmakro@posteo.de>
2019-06-29 22:57:45 +0200

    remove subnet for testing network

--- docker/testing/init.sh
+++ docker/testing/init.sh
@@ -8,10 +8,10 @@ RDSE_NET=$(docker network ls -q -f "name=$DOCKER_NETWORK")
 
 if [ -z "$RDSE_NET" ] 
 then
-      echo "testing network create"
-      docker network create --subnet 172.16.238.0/24 $DOCKER_NETWORK
+    echo "testing network create"
+    docker network create $DOCKER_NETWORK
 else
-      echo "testing network exists"
+    echo "testing network exists"
 fi
 
 # Setup log directory
@@ -28,10 +28,10 @@ echo "dataset copied to: $PATH_DATA_MOUNT/data.csv"
 
 if [ $MASTER_NEW_DATASET_SIZE_MB -gt $DATASET_SIZE_MB ] 
 then
-	echo "create bigger dataset of $MASTER_NEW_DATASET_SIZE_MB MB"
-	./testing/helper/create_dataset.sh $PATH_DATASET $PATH_DATA_MOUNT/data.csv $MASTER_NEW_DATASET_SIZE_MB
+    echo "create bigger dataset of $MASTER_NEW_DATASET_SIZE_MB MB"
+    ./testing/helper/create_dataset.sh $PATH_DATASET $PATH_DATA_MOUNT/data.csv $MASTER_NEW_DATASET_SIZE_MB
 else
-	cp $PATH_DATASET $PATH_DATA_MOUNT/data.csv
+    cp $PATH_DATASET $PATH_DATA_MOUNT/data.csv
 fi
 
 # We dont care about the gold standard when the dataset is replicated.
