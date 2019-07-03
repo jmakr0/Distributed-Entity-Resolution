4151828c0d2c4f48fc6f46f74a91180df42ed612
Maximilian Kroschewski <jmakro@posteo.de>
2019-07-03 11:54:54 +0200

    adjust the test for both cases

--- demos/transitive-closure/src/test/DFWTest.java
+++ demos/transitive-closure/src/test/DFWTest.java
@@ -93,10 +93,18 @@ public class DFWTest {
         // block size is bigger than matrix!
         DFW dfw = new DFW(testMatrix, 8);
 
-        // request pivot for calculation, dont calculate it, it's still pending
-        dfw.getBlock();
+        // request pivot for calculation
+        DFWBlock block = dfw.getBlock();
 
+        // pivot is still pending...
         Assert.assertFalse(dfw.calculated());
+
+        block.calculate();
+        dfw.dispatch(block.getTarget());
+
+        // Should done
+        Assert.assertTrue(dfw.calculated());
+
     }
 
 }
