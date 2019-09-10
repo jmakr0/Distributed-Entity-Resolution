2685e183d15545ba357323626620795ec11136b5
Nils Straßenburg <Nils.Strassenburg@student.hpi.uni-potsdam.de>
2019-09-10 22:54:53 +0200

    optimistic uses latest fw lib

--- approaches/optimistic/src/main/java/de/hpi/cluster/ClusterApp.java
+++ approaches/optimistic/src/main/java/de/hpi/cluster/ClusterApp.java
@@ -3,12 +3,8 @@ package de.hpi.cluster;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import com.beust.jcommander.Parameters;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class ClusterApp {
 
