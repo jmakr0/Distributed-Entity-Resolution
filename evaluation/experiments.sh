5ec95e6c447518c0343ff7766bc3d54553e62818
jmakr0 <jmakro@posteo.de>
2019-09-25 22:24:41 +0200

    Update experiments.sh

--- evaluation/experiments.sh
+++ evaluation/experiments.sh
@@ -6,20 +6,9 @@ experiment_nr=$3
 repeat=$4
 start_nodes=$5 # 0-indexed!
 max_worker_nodes=$6 # 0-indexed!
-
-# master=odin01
-# worker=all_worker
-# experiment_nr=5
-# repeat=5
-# start_nodes=4 # 0-indexed!
-# max_worker_nodes=11 # 0-indexed!
-
-ansible_ssh_pass=cluster
+ansible_ssh_pass=$7
 
 cd ansible
-
-# ansible-playbook  --ask-pass deram-experiments.yaml \
-#                   --extra-vars "master=thor01 worker=thor02 experiment_nr=1 repeat=2"
 
 for w in $(seq ${start_nodes} ${max_worker_nodes}); do
 
